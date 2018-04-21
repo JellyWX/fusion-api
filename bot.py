0996a05ce09c83cf3c77b4993c387635e356048c
jude-LafiteIII <judewrs@gmail.com>
2018-04-21 21:24:01 +0100

    added more handlers to catch other errors and such

--- bot.py
+++ bot.py
@@ -11,6 +11,16 @@ async def _help(ctx, *args):
 
 @bot.command()
 async def add(ctx, bot : discord.Member, owner : discord.Member, current_token=None):
+    with open('guilds', 'r') as f:
+        if ctx.guild.id not in [int(x) for x in f.read().split('\n') if x]:
+            raise Exception('Bot only designed for Fusion Discord Bots guild. Want an API for your own server? Contact me @JellyWX on Fusion Bots guild.')
+
+    if not bot.bot:
+        raise Exception('First mention must be a bot')
+
+    elif owner.bot:
+        raise Exception('Second mention must not be a bot')
+
     if current_token in ('--force', '-f') and not ctx.author.guild_permissions.administrator:
         ctx.send('Only admins may override')
 
@@ -30,5 +40,5 @@ async def add_error(ctx, error):
     await ctx.send('```reg add <bot mention> <owner mention>``` Need to replace a current token or change owner? Do ```reg add <bot mention> <owner mention> [current token]``` Admin and need to fix a mistake yourself? Do ```reg add <bot mention> <owner mention> --force```')
 
 
-with open('token') as f:
+with open('token', 'r') as f:
     bot.run(f.read().strip())
