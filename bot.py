4c76741f7e22e9f3de91fefbf3cdb9e57b8dbcdf
jude-Z87-HD3 <judewrs@gmail.com>
2018-05-08 16:55:32 +0100

    message stuff

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from make_bot import make_bot
 from discord.ext import commands
+import asyncio
 
 bot = commands.Bot(command_prefix='reg ')
 bot.remove_command('help')
@@ -27,18 +28,64 @@ async def add(ctx, bot : discord.Member, owner : discord.Member, current_token=N
     else:
         token, error = make_bot(bot.id, owner.id, current_token, current_token in ('--force', '-f'))
 
-        await ctx.send(embed=discord.Embed(description='''
+        try:
+            await ctx.author.send(embed=discord.Embed(description='''
 Token:
 ```{}```
 Notes:
 ```{}```
 '''.format(token, error)))
 
+        except discord.errors.Forbidden:
+            await ctx.send('Please enable direct messaging in your settings.')
+
 @add.error
 async def add_error(ctx, error):
     await ctx.send(error)
     await ctx.send('```reg add <bot mention> <owner mention>``` Need to replace a current token or change owner? Do ```reg add <bot mention> <owner mention> [current token]``` Admin and need to fix a mistake yourself? Do ```reg add <bot mention> <owner mention> --force```')
 
+
+async def search():
+    await bot.wait_until_ready()
+    print('Ready!')
+    print(bot.user.id)
+
+    channel = bot.get_channel(443439662959296523)
+
+    roles = {
+        'soundfx' : 443426944210698250,
+        'membercount' : 443427010124054530,
+        'loxantibadword' : 443426980210409472,
+        'applicationbot' : 443438462499291146,
+        'remindme' : None
+    }
+
+    while not bot.is_closed():
+        message = await channel.history(limit=1).next()
+
+        for r in message.reactions:
+
+            if isinstance(r.emoji, str) or (r.emoji.name not in roles.keys() or r.emoji.name == 'remindme'):
+                users = await r.users().flatten()
+                for user in users:
+                    await message.remove_reaction(r, user)
+
+                continue
+
+            role = [rol for rol in channel.guild.roles if rol.id == roles[r.emoji.name]][0]
+
+            users = await r.users().flatten()
+
+            for member in channel.guild.members:
+                if member in users and role not in member.roles:
+                    await member.add_roles(role)
+                elif role in member.roles:
+                    await member.remove_roles(role)
+                else:
+                    continue
+
+
+bot.loop.create_task(search())
 
 with open('token', 'r') as f:
     bot.run(f.read().strip())
